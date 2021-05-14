81250d3e3edd6a1914ae993f8862de07adf5ce2f
Michael Junio <michael.ray.junio@gmail.com>
2021-05-14 16:25:33 +1000

    Theme

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-
-import Header from '../Header';
-import ShoeIndex from '../ShoeIndex';
+import { useState } from 'react'
+import styled, { ThemeProvider } from 'styled-components/macro'
+import Header from '../Header'
+import ShoeIndex from '../ShoeIndex'
+import { QUERIES } from '../../constants'
 
 const App = () => {
-  const [sortId, setSortId] = React.useState('newest');
+  const [sortId, setSortId] = useState('newest')
 
   return (
-    <>
+    <ThemeProvider theme={{ QUERIES }}>
       <Header />
       <Main>
         <ShoeIndex sortId={sortId} setSortId={setSortId} />
       </Main>
-    </>
-  );
-};
+    </ThemeProvider>
+  )
+}
 
 const Main = styled.main`
   padding: 64px 32px;
-`;
+`
 
-export default App;
+export default App
--- src/constants.js
+++ src/constants.js
@@ -9,10 +9,22 @@ export const COLORS = {
   },
   primary: 'hsl(340deg 65% 47%)',
   secondary: 'hsl(240deg 60% 63%)',
-};
+}
 
 export const WEIGHTS = {
   normal: 500,
   medium: 600,
   bold: 800,
-};
+}
+
+export const BREAKPOINTS = {
+  phone: 37.5, // rem ~ 600px
+  tablet: 59.375, // rem ~ 950px
+  laptop: 81.25, // rem ~ 1300px
+}
+
+export const QUERIES = {
+  phone: `(max-width: ${BREAKPOINTS.phone}rem)`,
+  tablet: `(max-width: ${BREAKPOINTS.tablet}rem)`,
+  laptop: `(max-width: ${BREAKPOINTS.laptop}rem)`,
+}
