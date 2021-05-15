9a546c5d812ee3d9f98bdd13de6b14209009adab
Michael Junio <michael.ray.junio@gmail.com>
2021-05-15 15:24:56 +1000

    Prettier

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import styled, { ThemeProvider } from 'styled-components/macro'
 import Header from '../Header'
 import ShoeIndex from '../ShoeIndex'
-import { QUERIES } from '../../constants'
+import * as CONSTANTS from '../../constants'
 
 const App = () => {
   const [sortId, setSortId] = useState('newest')
 
   return (
-    <ThemeProvider theme={{ QUERIES }}>
+    <ThemeProvider theme={CONSTANTS}>
       <Header />
       <Main>
         <ShoeIndex sortId={sortId} setSortId={setSortId} />
--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,12 +1,5 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import {
-  Search,
-  Menu,
-  ShoppingBag,
-  ChevronDown,
-  X,
-} from 'react-feather';
+import styled from 'styled-components/macro'
+import { Search, Menu, ShoppingBag, ChevronDown, X } from 'react-feather'
 
 const icons = {
   search: Search,
@@ -14,28 +7,28 @@ const icons = {
   'shopping-bag': ShoppingBag,
   'chevron-down': ChevronDown,
   close: X,
-};
+}
 
 const Icon = ({ id, color, size, strokeWidth, ...delegated }) => {
-  const Component = icons[id];
+  const Component = icons[id]
 
   if (!Component) {
-    throw new Error(`No icon found for ID: ${id}`);
+    throw new Error(`No icon found for ID: ${id}`)
   }
 
   return (
     <Wrapper strokeWidth={strokeWidth} {...delegated}>
       <Component color={color} size={size} />
     </Wrapper>
-  );
-};
+  )
+}
 
 const Wrapper = styled.div`
   & > svg {
     display: block;
-    stroke-width: ${(p) =>
+    stroke-width: ${p =>
       p.strokeWidth !== undefined ? p.strokeWidth + 'px' : undefined};
   }
-`;
+`
 
-export default Icon;
+export default Icon
